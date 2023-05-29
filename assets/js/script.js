be79d86d8bc9cad98fde4f7b3cd349e9364e9617
ThaminduNR <thaminduniwantha777@gmail.com>
2023-05-29 13:30:44 +0530

    add the contact section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ $("#close-menu-btn").on("click", () => {
   $(".menu-view").css({ left: "-100%" });
   $(".menu-view").css({ transition: "0.4s" });
 });
+
+/* Typing Animation */
+
+var typing = new Typed(".develop", {
+  strings: [
+    "",
+    "UI/UX Designer",
+    "Freelancer",
+    "Graphics Designer",
+    "Web Designer",
+    "Front-End Developer",
+  ],
+  typeSpeed: 150,
+  backSpeed: 40,
+  loop: true,
+});
